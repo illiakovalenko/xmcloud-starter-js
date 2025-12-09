2035f83f1c757d1272f754dd4e5797bff82fbfde
Nipuna Abeywickrama <nipu@sitecore.net>
2025-12-09 12:06:59 +0530

    Update Article starter based on App router (#147)
    * article starter pages router example
    
    * Remove package-lock.json and clean up import map in kit-nextjs-article-starter-pages-router example
    
    * article starter app router update
    
    * Remove remaining rendering host configurations from xmcloud.build.json to speed up project build
    
    * Enhance hydration handling in ImageWrapper component and update RootLayout to suppress hydration warnings
    
    * Refactor request.ts to simplify message retrieval by directly assigning the result of getDictionary to messages
    
    * update package.json
    
    * remove article starter page router example project
    
    * add  AppPlaceholder component, Adjust package.json dependencies to use stable versions of Sitecore SDK packages.
    
    * Refactor components to use props for page context instead of useSitecore.
    
    * Refactor components to consistently import AppPlaceholder and streamline code formatting across various files.
    
    * Refactor Layout component to utilize Providers for context management, enhancing code organization and readability.
    
    * Refactor ArticleListing and Video components to use Next.js Image component for improved performance and layout consistency.
    
    * Refactor ImageWrapper component
    
    * Revert "Refactor ImageWrapper component"
    
    This reverts commit 4eb96f193148b5c6dc03e0c295caa697630cc57f.
    
    * Remove unused Roboto font configuration from layout component.
    
    * Improve AccordionBlock component by adding null checks for props to prevent potential runtime errors.
    
    * Make AccordionBlock component for client-side rendering support.
    
    * Update AccordionBlockDefault component to handle props destructuring more safely and improve className assignment.
    
    * Enhance AccordionFields interface by making fields and their properties optional, improving flexibility in data handling.
    
    * Refactor AccordionBlockDefault component for improved type safety and consistency.
    
    * Update AccordionBlockItem component to use optional chaining for safer prop access, enhancing robustness against undefined values.
    
    * Refactor VerticalImageAccordion component to utilize optional chaining for safer access to nested properties, enhancing stability against undefined values.
    
    * Integrate useSitecore hook in AccordionBlock component to streamline page editing state management, enhancing client-side rendering capabilities.
    
    * Add AccordionBlock component to component map and update its configuration for client-side rendering
    
    * Remove console log from AccordionBlock component
    
    * Update metadata fields to include Open Graph properties and ensure URL construction is environment-aware.

---
+++ examples/kit-nextjs-article-starter/src/Providers.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+import {
+  ComponentPropsCollection,
+  ComponentPropsContext,
+  Page,
+  SitecoreProvider,
+} from '@sitecore-content-sdk/nextjs';
+import scConfig from 'sitecore.config';
+import components from '.sitecore/component-map.client';
+import { ThemeProvider } from 'components/theme-provider/theme-provider.dev';
+import { VideoProvider } from './contexts/VideoContext';
+
+export default function Providers({
+  children,
+  page,
+  componentProps = {},
+}: {
+  children: React.ReactNode;
+  page: Page;
+  componentProps?: ComponentPropsCollection;
+}) {
+  return (
+    <SitecoreProvider api={scConfig.api} componentMap={components} page={page}>
+      <ComponentPropsContext value={componentProps}>
+        <VideoProvider>
+          <ThemeProvider attribute="class" disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </VideoProvider>
+      </ComponentPropsContext>
+    </SitecoreProvider>
+  );
+}
